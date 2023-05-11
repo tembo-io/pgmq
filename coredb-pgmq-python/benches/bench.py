e1cc6a65856de4cadadc8a3ecd79e0f3d4b72525
Adam Hendel <ChuckHend@users.noreply.github.com>
2023-05-11 15:47:57 -0500

    produce/consume bench (#319)

--- coredb-pgmq-python/benches/bench.py
+++ coredb-pgmq-python/benches/bench.py
@@ -1,244 +1,277 @@
 import random
 import time
+from typing import Optional
 
-import numpy as np
+import pandas as pd
 
 from coredb_pgmq_python import Message, PGMQueue
 
 
-def bench(host: str, port: str, username: str, num_iters: int = 1000, vt= 10) -> list[dict]:
-    rnd = random.randint(0, 100)
-    test_queue = f"bench_queue_{rnd}"
-    test_message = {"hello": "world"}
-
-    queue = PGMQueue(host=host, port=port, username=username, password="postgres", database="postgres")
-    try:
-        queue.create_queue(test_queue)
-    except Exception as e:
-        print("table exists?")
-
-    bench_0_start = time.time()
-
-    print(f"""
-    Starting benchmark
-    Total messages: {num_iters}
-    """)
-
-    writes = []
-    total_write_start = time.time()
-    # publish messages
-    print("Writing Messages")
-    for x in range(num_iters):
-        test_message["hello"] = x
-        start = time.time()
-        msg_id = queue.send(test_queue, test_message)
-        writes.append(time.time() - start)
-    total_write_duration = time.time() - total_write_start
-    print(f"total write time: {total_write_duration}")
-    write_results = summarize("writes", writes, host)
-
-    reads = []
-    total_read_start = time.time()
-    # read them all once, each
-    print("Reading Messages")
-    for x in range(num_iters):
-        start = time.time()
-        message: Message = queue.read(test_queue, vt = vt)
-        reads.append(time.time() - start)
-        if x % 100 == 0:
-            print(f"read {x} messages")
-
-    total_read_time = time.time() - total_read_start
-    print(f"total read time: {total_read_time}")
-    read_results = summarize("reads", reads, host)
-
-    # wait for all VT to expire
-    while time.time() - bench_0_start < vt:
-        print("waiting for all VTs to expire")
-        time.sleep(1)
-
-    # deletes
-    deletes = []
-    delete_start = time.time()
-    print("Deleting Messages")
-    for x in range(num_iters):
-        start = time.time()
-        queue.delete(test_queue, x)
-        deletes.append(time.time() - start)
-    total_delete_time = time.time() - delete_start
-    print(f"total delete time: {total_delete_time}")
-    delete_results = summarize("deletes", deletes, host)
-
-    # archives
-    print("Benchmarking: Archiving Messages")
-    writes = []
-    total_write_start = time.time()
-    # publish messages
-    print("Writing Messages")
-    for x in range(num_iters):
-        test_message["hello"] = x
-        start = time.time()
-        msg_id = queue.send(test_queue, test_message)
-        writes.append(time.time() - start)
-    total_write_duration = time.time() - total_write_start
-    print(f"total write time: {total_write_duration}")
-    summarize("writes", writes, host)
-
-    archives = []
-    archive_start = time.time()
-    print("Archiving Messages")
-    for x in range(num_iters):
-        start = time.time()
-        queue.archive(test_queue, x)
-        archives.append(time.time() - start)
-    total_archive_time = time.time() - archive_start
-    print(f"total archive time: {total_archive_time}")
-    archive_results = summarize("archives", archives, host)
-
+def bench_send(queue: PGMQueue, queue_name: str, msg: dict, num_messages: int) -> list[dict]:
+    all_msg_ids = []
+    write_start = time.time()
     results = []
-    results.append(write_results)
-    results.append(read_results)
-    results.append(delete_results)
-    results.append(archive_results)
-
+    print("Writing Messages")
+    for x in range(num_messages):
+        start = time.time()
+        msg_id = queue.send(queue_name, msg)
+        results.append({"operation": "write", "duration": time.time() - start, "msg_id": msg_id})
+        all_msg_ids.append(msg_id)
+        if (x + 1) % 10000 == 0:
+            print(f"write {x+1} messages")
+            elapsed = time.time() - write_start
+            avg_write = elapsed / (x + 1)
+            print(f"running avg write time (seconds): {avg_write}")
+    print(f"Sent {x+1} messages")
     return results
 
 
-def summarize(operation: str, timings: list[float], host: str) -> None:
-    total = len(timings)
-    mean = round(np.mean(timings), 4)
-    stdev = round(np.std(timings), 4)
-    _min = round(np.min(timings), 4)
-    _max = round(np.max(timings), 4)
-    print(f"Summary: {operation}")
-    print(f"Count: {total}, mean: {mean}, stdev: {stdev}, min: {_min}, max: {_max}")
-    return {
-        "host": host,
-        "operation": operation,
-        "count": total,
-        "mean": mean,
-        "stdev": stdev,
-        "min": _min,
-        "max": _max
-    }
+def bench_read_archive(queue: PGMQueue, queue_name: str, num_messages: int) -> list[dict]:
+    """Benchmarks the read and archive of messages"""
+    read_elapsed = 0.0
+    archive_elapsed = 0.0
+    results = []
+    for x in range(num_messages):
+        read_start = time.time()
+        message: Message = queue.read(queue_name, vt=2)  # type: ignore
+        read_duration = time.time() - read_start
+        results.append({"operation": "read", "duration": read_duration, "msg_id": message.msg_id})
+        read_elapsed += read_duration
+
+        archive_start = time.time()
+        queue.archive(queue_name, message.msg_id)
+        archive_duration = time.time() - archive_start
+        results.append({"operation": "archive", "duration": archive_duration, "msg_id": message.msg_id})
+        archive_elapsed += archive_duration
+
+        if (x + 1) % 10000 == 0:
+            avg_read = read_elapsed / (x + 1)
+            print(f"read {x+1} messages, avg read time (seconds): {avg_read}")
+            avg_archive = archive_elapsed / (x + 1)
+            print(f"archived {x+1} messages, avg archive time (seconds): {avg_archive}")
+    print(f"Read {x+1} messages")
+    return results
 
 
+def bench_line_item(
+    host: str,
+    port: str,
+    username: str = "postgres",
+    num_messages: int = 10000,
+    vt=10,
+    password: str = "postgres",
+    database: str = "postgres",
+    partition_interval: int = 10000,
+    retention_interval: Optional[int] = None,
+) -> list[dict]:
+    """records each transaction as a separate line item. Captures results into a list.
 
-
-def bench_line_item(host: str, port: str, username: str, num_iters: int = 1000, vt= 10) -> list[dict]:
-    """logs each transaction as a separate line
-    
     returns:
             [{
                 "operation": <operation>,
-                "duration": time.time() - start,
-                "msg_id": msg_id
+                "duration": <duration, in seconds>,
+                "msg_id": <msg_id>
             }]
     """
     rnd = random.randint(0, 100)
     test_queue = f"bench_queue_{rnd}"
+    print(f"Test queue: {test_queue}")
+
     test_message = {"hello": "world"}
     bench_0_start = time.time()
-
-    queue = PGMQueue(host=host, port=port, username=username, password="postgres", database="postgres")
+    queue = PGMQueue(host=host, port=port, username=username, password=password, database=database)
     try:
-        queue.create_queue(test_queue)
+        print(f"Queue retention: {retention_interval}")
+        if retention_interval is None:
+            print("Defaulting to retaining all messages: {}")
+            retention_interval = num_messages
+        queue.create_queue(test_queue, partition_interval=partition_interval, retention_interval=retention_interval)
     except Exception as e:
-        print("table exists?")
+        print(f"{e}")
 
-
-    print(f"""
+    print(
+        f"""
     Starting benchmark
-    Total messages: {num_iters}
-    """)
-
-
+    Total messages: {num_messages}
+    """
+    )
 
     total_results = []
 
     # publish messages
-    print("Writing Messages")
-    all_msg_ids = []
-    for x in range(num_iters):
-        test_message["hello"] = x
-        start = time.time()
-        msg_id = queue.send(test_queue, test_message)
-        total_results.append(
-            {
-                "operation": "write",
-                "duration": time.time() - start,
-                "msg_id": msg_id
-            }
-        )
-        all_msg_ids.append(msg_id)
+    write_results: list[dict] = bench_send(queue, test_queue, test_message, num_messages)
+    total_results.extend(write_results)
 
     # read them all once, each
     print("Reading Messages")
-    for x in range(num_iters):
-        start = time.time()
-        message: Message = queue.read(test_queue, vt = vt)
-        if x % 100 == 0:
-            print(f"read {x} messages")
-        total_results.append(
-            {
-                "operation": "read",
-                "duration": time.time() - start,
-                "msg_id": message.msg_id
-            }
-        )
+    read_arch_results: list[dict] = bench_read_archive(queue, test_queue, num_messages)
+    total_results.extend(read_arch_results)
 
     # wait for all VT to expire
     while time.time() - bench_0_start < vt:
         print("waiting for all VTs to expire")
         time.sleep(2)
 
-    # deletes
-    print("Deleting Messages")
-    for x in all_msg_ids:
-        start = time.time()
-        queue.delete(test_queue, x)
-        total_results.append(
-            {
-                "operation": "delete",
-                "duration": time.time() - start,
-                "msg_id": x
-            }
-        )
-
-    # archives
-    print("Benchmarking: Archiving Messages")
+    print("Benchmarking: Message Deletion")
     all_msg_ids = []
     # publish messages
-    for x in range(num_iters):
-        test_message["hello"] = x
+    for x in range(num_messages):
         start = time.time()
         msg_id = queue.send(test_queue, test_message)
         all_msg_ids.append(msg_id)
 
-
-    print("Archiving Messages")
+    print("Deleting Messages")
     for x in all_msg_ids:
         start = time.time()
-        queue.archive(test_queue, x)
-        total_results.append(
-            {
-                "operation": "archive",
-                "duration": time.time() - start,
-                "msg_id": x
-            }
-        )
+        queue.delete(test_queue, x)
+        total_results.append({"operation": "delete", "duration": time.time() - start, "msg_id": x})
 
     return total_results
 
-if __name__ == "__main__":
-    n_messages = 100
-    trials = [
-        ("localhost", 5432, "postgres", n_messages),     # docker
-        # ("local", 28815, "username")    # pgrx 
-    ]
+
+def produce(
+    queue_name: str,
+    csv_name: str,
+    connection_info: dict,
+    duration_seconds: int = 60,
+    tps: int = 500,
+):
+    """Publishes messages at a given rate for a given duration
+    Assumes queue_name already exists. Writes results to csv.
+
+    Args:
+        queue_name: The name of the queue to publish to
+        csv_name: The name of the csv to write results to
+        duration_seconds: The number of seconds to publish messages
+        tps: The number of messages to publish per second
+    """
+    queue = PGMQueue(**connection_info)
+
+    msg = {"hello": "world"}
+    # delay between messages, assuming instantaneous send
+    delay = 1.0 / tps
+
+    total_messages = tps * duration_seconds
+
+    start_time = time.time()
+
     all_results = []
-    for t in trials:
+    for _ in range(int(total_messages)):
+        send_start = time.time()
+        msg_id: int = queue.send(queue_name, msg)
+        send_duration = time.time() - send_start
         all_results.append(
-            bench(t[0], t[1], t[2], t[3])
+            {
+                "operation": "write",
+                "duration": round(send_duration, 4),
+                "msg_id": msg_id,
+            }
         )
-    print(all_results)
+        # Sleep to maintain the desired tps
+        time.sleep(delay - ((time.time() - start_time) % delay))
+
+    df = pd.DataFrame(all_results)
+    df.to_csv(csv_name, index=False)
+
+
+def consume(queue_name: str, csv_name: str, connection_info: dict):
+    """Consumes messages from a queue and archives them. Writes results to csv.
+
+    Halts consumption after 5 seconds of no messages.
+    """
+    queue = PGMQueue(**connection_info)
+
+    results = []
+    no_message_timeout = 0
+    while no_message_timeout < 5:
+        read_start = time.time()
+        message: Optional[Message] = queue.read(queue_name, vt=10)
+        if message is None:
+            no_message_timeout += 1
+            print(f"No message -- {no_message_timeout}, sleeping 1 second")
+            time.sleep(1)
+            continue
+        else:
+            no_message_timeout = 0
+        read_duration = time.time() - read_start
+        results.append({"operation": "read", "duration": read_duration, "msg_id": message.msg_id})
+
+        archive_start = time.time()
+        queue.archive(queue_name, message.msg_id)
+        archive_duration = time.time() - archive_start
+        results.append({"operation": "archive", "duration": archive_duration, "msg_id": message.msg_id})
+
+    print(f"Consumed {len(results)} messages")
+    df = pd.DataFrame(results)
+    print(f"Writing results to {csv_name}")
+    df.to_csv(csv_name, index=False)
+
+
+def summarize(csv_1: str, csv_2: str, results_file: str, duration_seconds: int, tps: int):
+    """summarizes results from two csvs into pdf"""
+    df1 = pd.read_csv(csv_1)
+    df2 = pd.read_csv(csv_2)
+
+    df = pd.concat([df1, df2])
+
+    _num_df = df[df["operation"] == "archive"]
+    num_messages = _num_df.shape[0]
+    # convert seconds to milliseconds
+    df["duration"] = df["duration"] * 1000
+
+    for op in ["read", "archive", "write"]:
+        _df = df[df["operation"] == op]
+        bbplot = _df.boxplot(
+            column="duration", by="operation", fontsize=12, layout=(2, 1), rot=90, figsize=(25, 20), return_type="axes"
+        )
+
+        bbplot[0].set_ylabel("Milliseconds")
+
+        title = f"""
+        num_messages = {num_messages}
+        duration = {duration_seconds}
+        tps = {tps}
+        """
+        bbplot[0].set_title(title)
+
+        filename = f"{op}_{results_file}"
+        bbplot[0].get_figure().savefig(filename)
+        print("Saved: ", filename)
+
+
+if __name__ == "__main__":
+    # run the multiproc benchmark
+    # 1 process publishing messages
+    # another process reading and archiving messages
+    # both write results to csv
+    # script merges csvs and summarizes results
+    from multiprocessing import Process
+
+    rnd = random.randint(0, 1000)
+    test_queue = f"bench_queue_{rnd}"
+    connection_info = dict(host="localhost", port=28815, username="postgres", password="postgres", database="postgres")
+    queue = PGMQueue(**connection_info)  # type: ignore
+    print(f"Creating queue: {test_queue}")
+    queue.create_queue(test_queue, partition_interval=10000, retention_interval=100000)
+
+    produce_csv = f"produce_{test_queue}.csv"
+    consume_csv = f"consume_{test_queue}.csv"
+
+    # run producing and consuming in parallel, separate processes
+    duration_seconds = 60 * 60  # 1 hour
+    tps = 550  # max transactions per second (producing)
+    proc_produce = Process(target=produce, args=(test_queue, produce_csv, connection_info, duration_seconds, tps))
+    proc_produce.start()
+
+    proc_consume = Process(target=consume, args=(test_queue, consume_csv, connection_info))
+    proc_consume.start()
+
+    print("Waiting for processes to finish")
+    proc_consume.join()
+
+    # once consuming finishes, summarize
+    results_file = f"results_{test_queue}.jpg"
+    # TODO: organize results in a directory or something, log all the params
+    summarize(
+        csv_1=produce_csv, csv_2=consume_csv, results_file=results_file, duration_seconds=duration_seconds, tps=tps
+    )
--- coredb-pgmq-python/coredb_pgmq_python/queue.py
+++ coredb-pgmq-python/coredb_pgmq_python/queue.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Optional, Union
+from typing import Optional
 
 from psycopg.types.json import Jsonb
 from psycopg_pool import ConnectionPool
@@ -47,17 +47,23 @@ class PGMQueue:
         with self.pool.connection() as conn:
             conn.execute("create extension if not exists pgmq cascade;")
 
-    def create_queue(self, queue: str) -> None:
-        """Create a queue"""
-        with self.pool.connection() as conn:
-            conn.execute("select pgmq_create(%s);", [queue])
+    def create_queue(self, queue: str, partition_interval: int = 10000, retention_interval: int = 100000) -> None:
+        """Create a new queue
 
-    def create_(
-        self, queue: str, partition_interval: Optional[str] = "daily", retention_interval: Optional[str] = "5 days"
-    ) -> None:
-        """Create a partitioned queue"""
+        Note: Partitions are created pg_partman which must be configured in postgresql.conf
+            Set `pg_partman_bgw.interval` to set the interval for partition creation and deletion.
+            A value of 10 will create new/delete partitions every 10 seconds. This value should be tuned
+            according to the volume of messages being sent to the queue.
+
+        Args:
+            queue: The name of the queue.
+            partition_interval: The number of messages per partition. Defaults to 10,000.
+            retention_interval: The number of messages to retain. Messages exceeding this number will be dropped.
+                Defaults to 100,000.
+        """
+
         with self.pool.connection() as conn:
-            conn.execute("select pgmq_create_non_partitioned(%s, %s);", [queue, partition_interval, retention_interval])
+            conn.execute("select pgmq_create(%s, %s::text, %s::text);", [queue, partition_interval, retention_interval])
 
     def send(self, queue: str, message: dict, delay: Optional[int] = None) -> int:
         """Send a message to a queue"""
@@ -72,13 +78,20 @@ class PGMQueue:
             ).fetchall()
         return message[0][0]
 
-    def read(self, queue: str, vt: Optional[int] = None, limit: int = 1) -> Union[Message, list[Message]]:
+    def read(self, queue: str, vt: Optional[int] = None) -> Optional[Message]:
         """Read a message from a queue"""
         with self.pool.connection() as conn:
-            rows = conn.execute("select * from pgmq_read(%s, %s, %s);", [queue, vt or self.vt, limit]).fetchall()
+            rows = conn.execute("select * from pgmq_read(%s, %s, %s);", [queue, vt or self.vt, 1]).fetchall()
 
         messages = [Message(msg_id=x[0], read_ct=x[1], enqueued_at=x[2], vt=x[3], message=x[4]) for x in rows]
-        return messages[0] if len(messages) == 1 else messages
+        return messages[0] if len(messages) == 1 else None
+
+    def read_batch(self, queue: str, vt: Optional[int] = None, batch_size=1) -> Optional[list[Message]]:
+        """Read abatch of messages from a queue"""
+        with self.pool.connection() as conn:
+            rows = conn.execute("select * from pgmq_read(%s, %s, %s);", [queue, vt or self.vt, batch_size]).fetchall()
+
+        return [Message(msg_id=x[0], read_ct=x[1], enqueued_at=x[2], vt=x[3], message=x[4]) for x in rows]
 
     def pop(self, queue: str) -> Message:
         """Read a message from a queue"""
