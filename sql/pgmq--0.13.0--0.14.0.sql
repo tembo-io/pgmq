dd57a5dfea301f861619313eb17fd312b384ec76
Adam Hendel <adam@tembo.io>
2023-08-16 17:11:35 -0500

    update migration script

--- sql/pgmq--0.13.0--0.14.0.sql
+++ sql/pgmq--0.13.0--0.14.0.sql
@@ -5,6 +5,7 @@ BEGIN
     FOR table_name IN (SELECT queue_name FROM public.pgmq_meta)
     LOOP
         EXECUTE format('ALTER EXTENSION pgmq ADD TABLE %I', 'pgmq_' || table_name);
+        EXECUTE format('ALTER EXTENSION pgmq ADD TABLE %I', 'pgmq_' || table_name, '_archive');
         EXECUTE format('ALTER TABLE %I set SCHEMA pgmq', 'pgmq_' || table_name);
     END LOOP;
 ALTER TABLE public.pgmq_meta set SCHEMA pgmq;
