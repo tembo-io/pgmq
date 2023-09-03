f6e831e91df2431620ec558f5eb588188c35efa6
Craig Pastro <craig.pastro@pangea.cloud>
2023-09-03 12:15:53 -0700

    update migration

--- sql/pgmq--0.21.0--0.22.0.sql
+++ sql/pgmq--0.21.0--0.22.0.sql
@@ -1,18 +1,24 @@
-ALTER TABLE pgmq_meta ADD COLUMN is_partitioned BOOLEAN;
-
 DO $$
 DECLARE
+    partitioned_queues TEXT;
     qname TEXT;
 BEGIN
-    FOR qname IN (SELECT queue_name FROM pgmq_meta)
-    LOOP
-        -- If qname is in public.part_config it must be a partitioned table.
-        IF format('public.pgmq_%1$I', qname) IN (SELECT parent_table FROM public.part_config) THEN
-            UPDATE pgmq_meta SET is_partitioned = true WHERE queue_name = qname;
-        ELSE
-            UPDATE pgmq_meta SET is_partitioned = false WHERE queue_name = qname;
-        END IF;
-    END LOOP;
+    ALTER TABLE pgmq_meta ADD COLUMN is_partitioned BOOLEAN;
+
+    IF 'pg_partman' NOT IN (SELECT extname FROM pg_extension) THEN
+        UPDATE pgmq_meta SET is_partitioned = false;
+    ELSE
+        FOR qname IN (SELECT queue_name FROM pgmq_meta)
+        LOOP
+            -- If qname is in public.part_config it must be a partitioned table.
+            IF format('public.pgmq_%1$I', qname) IN (SELECT parent_table FROM public.part_config) THEN
+                UPDATE pgmq_meta SET is_partitioned = true WHERE queue_name = qname;
+            ELSE
+                UPDATE pgmq_meta SET is_partitioned = false WHERE queue_name = qname;
+            END IF;
+        END LOOP;
+    END IF;
+
+    ALTER TABLE pgmq_meta ALTER COLUMN is_partitioned SET NOT NULL;
 END $$;
 
-ALTER TABLE pgmq_meta ALTER COLUMN is_partitioned SET NOT NULL;
