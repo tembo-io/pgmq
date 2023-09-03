ae8daae7b6f20e720cc586474ae152ced40b71bf
Craig Pastro <craig.pastro@pangea.cloud>
2023-09-03 13:45:06 -0700

    try

---
+++ sql/pgmq--0.21.0--0.22.0.sql
@@ -0,0 +1,28 @@
+ALTER EXTENSION pgmq DROP FUNCTION pgmq_delete(TEXT, bigint[]);
+DROP FUNCTION pgmq_delete(TEXT, bigint[]);
+
+-- src/lib.rs:215
+-- pgmq::pgmq_delete
+CREATE FUNCTION "pgmq_delete"(
+	"queue_name" TEXT, /* &str */
+	"msg_ids" bigint[] /* alloc::vec::Vec<i64> */
+) RETURNS TABLE (
+	"pgmq_delete" bool  /* bool */
+)
+STRICT 
+LANGUAGE c /* Rust */
+AS 'MODULE_PATHNAME', 'pgmq_delete_batch_wrapper';
+
+ALTER EXTENSION pgmq ADD FUNCTION pgmq_delete(TEXT, bigint[]);
+
+-- -- src/lib.rs:260
+-- -- pgmq::pgmq_archive
+-- CREATE  FUNCTION "pgmq_archive"(
+-- 	"queue_name" TEXT, /* &str */
+-- 	"msg_ids" bigint[] /* alloc::vec::Vec<i64> */
+-- ) RETURNS TABLE (
+-- 	"pgmq_archive" bool  /* bool */
+-- )
+-- STRICT 
+-- LANGUAGE c /* Rust */
+-- AS 'MODULE_PATHNAME', 'pgmq_archive_batch_wrapper';
