55c061825d9efc44663b6d0f09fae3f466803343
Craig Pastro <craig.pastro@pangea.cloud>
2023-09-11 11:23:53 -0700

    Update pgmq--0.24.0--0.24.1.sql

--- sql/pgmq--0.24.0--0.24.1.sql
+++ sql/pgmq--0.24.0--0.24.1.sql
@@ -1,22 +1,16 @@
 DO $$
 DECLARE
     qname TEXT;
-    seqname TEXT;
+    l_value BIGINT;
 BEGIN
     FOR qname IN (SELECT queue_name FROM pgmq_meta)
     LOOP
         EXECUTE format('ALTER TABLE public.pgmq_%I ALTER COLUMN msg_id DROP DEFAULT;', qname);
         EXECUTE format('ALTER TABLE public.pgmq_%I_archive ALTER COLUMN msg_id DROP DEFAULT;', qname);
-    END LOOP;
-
-    FOR seqname IN (SELECT relname FROM pg_class WHERE relkind = 'S' AND relname LIKE 'pgmq_%')
-    LOOP
-        EXECUTE format('DROP SEQUENCE %I CASCADE;', seqname);
-    END LOOP;
-
-    FOR qname IN (SELECT queue_name FROM pgmq_meta)
-    LOOP
+        EXECUTE format('SELECT last_value FROM public.pgmq_%I_msg_id_seq;', qname) INTO l_value;
+        EXECUTE format('DROP SEQUENCE public.pgmq_%I_msg_id_seq CASCADE;', qname);
         EXECUTE format('ALTER TABLE public.pgmq_%I ALTER COLUMN msg_id ADD GENERATED ALWAYS AS IDENTITY;', qname);
+        EXECUTE format('SELECT setval(pg_get_serial_sequence(''public.pgmq_%I'', ''msg_id''), %s);', qname, l_value);
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
