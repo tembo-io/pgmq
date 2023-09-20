274bc0e0afa609eacd93f028a3c6cff1dfb00b22
v0idpwn <v0idpwn@gmail.com>
2023-09-20 13:43:09 +0300

    Drop archive

--- sql/pgmq--0.25.0--0.26.0.sql
+++ sql/pgmq--0.25.0--0.26.0.sql
@@ -1,6 +1,8 @@
 -- Dropping rust impls
 DROP FUNCTION pgmq.read(text, integer, integer);
 DROP FUNCTION pgmq.read_with_poll(text, integer, integer, integer, integer);
+DROP FUNCTION pgmq.archive(text, bigint);
+DROP FUNCTION pgmq.archive(text, bigint[]);
 
 -- Adding message record type, required for the functions
 CREATE TYPE pgmq.message_record AS (
