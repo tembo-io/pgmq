c603d1cd83bbec18f68a61e2fdef61d920afa211
v0idpwn <v0idpwn@gmail.com>
2023-09-25 23:18:29 +0300

    Add PL/pgSQL implementations of send and send_batch

--- src/sql_src.sql
+++ src/sql_src.sql
@@ -234,3 +234,47 @@ BEGIN
     RETURN QUERY EXECUTE sql USING msg_id;
 END;
 $$ LANGUAGE plpgsql;
+
+-- send
+-- sends a message to a queue, optionally with a delay
+CREATE FUNCTION pgmq.send(
+    queue_name TEXT,
+    msg JSONB,
+    delay INTEGER DEFAULT 0
+) RETURNS SETOF BIGINT AS $$
+DECLARE
+    sql TEXT;
+BEGIN
+    sql := FORMAT(
+        $QUERY$
+        INSERT INTO pgmq.q_%s (vt, message)
+        VALUES ((clock_timestamp() + interval '%s seconds'), $1)
+        RETURNING msg_id;
+        $QUERY$,
+        queue_name, delay
+    );
+    RETURN QUERY EXECUTE sql USING msg;
+END;
+$$ LANGUAGE plpgsql;
+
+-- send_batch
+-- sends an array of list of messages to a queue, optionally with a delay
+CREATE FUNCTION pgmq.send_batch(
+    queue_name TEXT,
+    msgs JSONB[],
+    delay INTEGER DEFAULT 0
+) RETURNS SETOF BIGINT AS $$
+DECLARE
+    sql TEXT;
+BEGIN
+    sql := FORMAT(
+        $QUERY$
+        INSERT INTO pgmq.q_%s (vt, message)
+        SELECT clock_timestamp() + interval '%s seconds', unnest($1)
+        RETURNING msg_id;
+        $QUERY$,
+        queue_name, delay
+    );
+    RETURN QUERY EXECUTE sql USING msgs;
+END;
+$$ LANGUAGE plpgsql;
