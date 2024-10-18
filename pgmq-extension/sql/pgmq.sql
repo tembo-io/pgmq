bb6317904f14b148b733df2c8222c76903b54812
Abinand P <abinand0911@gmail.com>
2024-10-18 22:25:57 +0530

    updated and resolved the function name to get the table

--- pgmq-extension/sql/pgmq.sql
+++ pgmq-extension/sql/pgmq.sql
@@ -378,26 +378,21 @@ END
 $$ LANGUAGE plpgsql;
 
 -- purge queue, deleting all entries in it.
-CREATE OR REPLACE FUNCTION pgmq.purge(
-    queue_name text
-)
-RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION pgmq."purge_queue"(queue_name TEXT)
+RETURNS BIGINT AS $$
 DECLARE
-    qtable text;
-    deleted_count integer;
+  deleted_count INTEGER;
+  qtable TEXT := pgmq.format_table_name(queue_name, 'q');
 BEGIN
-    -- Determine the name of the queue table
-    SELECT INTO qtable pgmq.get_queue_table_name(queue_name);
+  -- Get the row count before truncating
+  EXECUTE format('SELECT count(*) FROM pgmq.%I', qtable) INTO deleted_count;
 
-    -- Get the row count before truncating
-    EXECUTE format('SELECT count(*) FROM pgmq.%I', qtable) INTO deleted_count;
-    
-    -- Use TRUNCATE for better performance on large tables
-    EXECUTE format('TRUNCATE pgmq.%I', qtable);
+  -- Use TRUNCATE for better performance on large tables
+  EXECUTE format('TRUNCATE TABLE pgmq.%I', qtable);
 
-    -- Return the number of purged rows
-    RETURN deleted_count;
-END;
+  -- Return the number of purged rows
+  RETURN deleted_count;
+END
 $$ LANGUAGE plpgsql;
 
 
