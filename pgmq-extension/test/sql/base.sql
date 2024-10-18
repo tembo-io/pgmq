5c77ffc88c66efc520cc1061c0613036c35dc867
Neptune <neptune650@proton.me>
2024-10-18 14:38:07 -0500

    Add test for send_at

--- pgmq-extension/test/sql/base.sql
+++ pgmq-extension/test/sql/base.sql
@@ -45,6 +45,17 @@ SELECT msg_id = :msg_id FROM pgmq.set_vt('test_default_queue', :msg_id, 0);
 -- read again, should have msg_id 1 again
 SELECT msg_id = :msg_id FROM pgmq.read('test_default_queue', 2, 1);
 
+SELECT pgmq.create('test_default_queue_vt');
+
+-- send message with timestamp
+SELECT * from pgmq.send_at('test_default_queue_vt', '{"hello": "world"}', CURRENT_TIMESTAMP + '5 seconds'::interval);
+
+-- read, assert no messages because we set timestamp to the future
+SELECT msg_id = :msg_id FROM pgmq.read('test_default_queue_vt', 2, 1);
+
+-- read again, now using poll to block until message is ready
+SELECT msg_id = :msg_id FROM pgmq.read_with_poll('test_default_queue_vt', 10, 1, 10);
+
 -- send a batch of 2 messages
 SELECT pgmq.create('batch_queue');
 SELECT ARRAY( SELECT pgmq.send_batch(
