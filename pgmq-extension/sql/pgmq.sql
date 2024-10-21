d74f771e0c6e417aaefd1e5b62506bfa194d35c1
Neptune <neptune650@proton.me>
2024-10-20 19:17:46 -0500

    Avoid code duplication in drop_queue

--- pgmq-extension/sql/pgmq.sql
+++ pgmq-extension/sql/pgmq.sql
@@ -538,55 +538,7 @@ BEGIN
         queue_name
     ) INTO partitioned;
 
-    EXECUTE FORMAT(
-        $QUERY$
-        ALTER EXTENSION pgmq DROP TABLE pgmq.%I
-        $QUERY$,
-        qtable
-    );
-
-    EXECUTE FORMAT(
-        $QUERY$
-        ALTER EXTENSION pgmq DROP TABLE pgmq.%I
-        $QUERY$,
-        atable
-    );
-
-    EXECUTE FORMAT(
-        $QUERY$
-        DROP TABLE IF EXISTS pgmq.%I
-        $QUERY$,
-        qtable
-    );
-
-    EXECUTE FORMAT(
-        $QUERY$
-        DROP TABLE IF EXISTS pgmq.%I
-        $QUERY$,
-        atable
-    );
-
-     IF EXISTS (
-          SELECT 1
-          FROM information_schema.tables
-          WHERE table_name = 'meta' and table_schema = 'pgmq'
-     ) THEN
-        EXECUTE FORMAT(
-            $QUERY$
-            DELETE FROM pgmq.meta WHERE queue_name = %L
-            $QUERY$,
-            queue_name
-        );
-     END IF;
-
-     IF partitioned THEN
-        EXECUTE FORMAT(
-          $QUERY$
-          DELETE FROM %I.part_config where parent_table in (%L, %L)
-          $QUERY$,
-          pgmq._get_pg_partman_schema(), fq_qtable, fq_atable
-        );
-     END IF;
+    PERFORM pgmq.drop_queue(queue_name, partitioned);
 
     RETURN TRUE;
 END;
