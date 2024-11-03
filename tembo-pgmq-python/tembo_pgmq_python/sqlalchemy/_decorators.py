23282c5376fc0365f29c6b26a4a11e7636104318
jason810496 <zhu424.dev@gmail.com>
2024-11-03 22:58:42 +0800

    refactor: check "session" in kwargs and transaction_mode

--- tembo-pgmq-python/tembo_pgmq_python/sqlalchemy/_decorators.py
+++ tembo-pgmq-python/tembo_pgmq_python/sqlalchemy/_decorators.py
@@ -4,13 +4,16 @@ import functools
 if TYPE_CHECKING:
     from tembo_pgmq_python.sqlalchemy.queue import PGMQueue
 
+
 def inject_session(func):
     """Decorator to inject sqlalchemy session into a method if session not provided."""
 
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        self:"PGMQueue" = args[0]
-        if "session" not in kwargs:
+        self: "PGMQueue" = args[0]
+        if "session" not in kwargs or kwargs["session"] is None:
+            if self.transaction_mode:
+                raise Exception("Session must be provided in transaction mode")
             session = self.session_maker()
             kwargs["session"] = session
             try:
@@ -18,28 +21,32 @@ def inject_session(func):
                 return result
             except Exception as e:
                 session.rollback()
-                raise
+                raise e
             finally:
                 session.close()
-
+        else:
+            return func(*args, **kwargs)
 
     return wrapper
+
 
 def inject_async_session(func):
     """Asynchronous decorator to inject sqlalchemy session into a method if session not provided."""
 
     @functools.wraps(func)
     async def wrapper(self, *args, **kwargs):
-        self:"PGMQueue" = args[0]
-        if "session" not in kwargs:
+        self: "PGMQueue" = args[0]
+        if "session" not in kwargs or kwargs["session"] is None:
+            if self.transaction_mode:
+                raise Exception("AsyncSession must be provided in transaction mode")
             session = await self.session_maker()
             kwargs["session"] = session
             try:
                 result = await func(self, *args, **kwargs)
                 return result
             except Exception as e:
-                session.rollback()
-                raise
+                await session.rollback()
+                raise e
             finally:
                 session.close()
 
