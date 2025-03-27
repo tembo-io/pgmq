40c991c06ea9f4d6ec032cc703f9bc7ca9e49a70
Adam Hendel <hendel.adam@gmail.com>
2025-03-26 21:34:46 -0500

    fix queue name length check

--- pgmq-extension/sql/pgmq.sql
+++ pgmq-extension/sql/pgmq.sql
@@ -678,8 +678,12 @@ $$ LANGUAGE plpgsql;
 CREATE FUNCTION pgmq.validate_queue_name(queue_name TEXT)
 RETURNS void AS $$
 BEGIN
-  IF length(queue_name) >= 48 THEN
-    RAISE EXCEPTION 'queue name is too long, maximum length is 48 characters';
+  IF length(queue_name) > 47 THEN
+    -- complete table identifier must be <= 63
+    -- https://www.postgresql.org/docs/17/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS
+    -- e.g. template_pgmq_q_my_queue is an identifier for my_queue when partitioned
+    -- template_pgmq_q_ (16) + <a max length queue name> (47) = 63 
+    RAISE EXCEPTION 'queue name is too long, maximum length is 47 characters';
   END IF;
 END;
 $$ LANGUAGE plpgsql;
